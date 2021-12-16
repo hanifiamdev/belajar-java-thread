154be22ebb85a9a8132eda1c8849e3f479f45ad4
hanifiamdev <hanifamrullahdev@gmail.com>
2021-12-16 12:38:46 +0700

    Inisialisasi Project

---
+++ src/main/java/com/hanifiamdev/thread/BelajarJavaThreadApplication.java
@@ -0,0 +1,13 @@
+package com.hanifiamdev.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaThreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanifiamdev/thread/BelajarJavaThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanifiamdev.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
