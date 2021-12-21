d80ee62032983d61994ae5c425166503972da45a
hanifiamdev <hanifamrullahdev@gmail.com>
2021-12-21 17:40:34 +0700

    Exchager

---
+++ src/test/java/com/hanifiamdev/thread/ExchangerTest.java
@@ -0,0 +1,40 @@
+package com.hanifiamdev.thread;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExchangerTest {
+
+    @Test
+    void test() throws InterruptedException {
+
+        final var exchanger = new Exchanger<String>();
+        final var executor = Executors.newFixedThreadPool(10);
+
+
+        executor.execute(() -> {
+            try {
+                System.out.println("Thread 1 : Send : First");
+                var result = exchanger.exchange("First");
+                System.out.println("Thread 1 : Receive : " + result);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        executor.execute(() -> {
+            try {
+                System.out.println("Thread 2 : Send : Second");
+                var result = exchanger.exchange("Second");
+                System.out.println("Thread 2 : Receive : " + result);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
